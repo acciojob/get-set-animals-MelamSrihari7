d2718c187e6edb09a15b759bc692083429a0c2b9
MelamSrihari7 <melamsrihari@gmail.com>
2025-09-01 13:36:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Animal {
         return this._species;
     }
     makeSound() {
-        console.log(`The ${this._species} makes a sound.`);
+        console.log(`The ${this._species} makes a sound`);
     }
 }
 
